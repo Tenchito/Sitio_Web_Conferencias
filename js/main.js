a0af60f8d1ffd0c8575eb422e22b5ea7ccdff02e
Tenchito <alan_lopez.1@hotmail.com>
2020-12-01 10:57:55 -0600

    Creacion de variables de campos en main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 (function(){
     "use strict";
     document.addEventListener('DOMContentLoaded', function(){
-        console.log("Listo para trabajar");
+        // Campos datos de usuario
+        var nombre = document.getElementById('nombre');
+        var apellido = document.getElementById('apellido');
+        var email = document.getElementById('email');
+
+        // Campos de pases
+        var pase_dia = document.getElementById('pase_dia');
+        var pase_dosdias = document.getElementById('pase_dosdias');
+        var pase_completo = document.getElementById('pase_completo');
+
+        // Botones y divs
+        var calcular = document.getElementById('calcular');
+        var errorDiv = document.getElementById('error');
+        var botonRegistro = document.getElementById('btnRegistro');
+        var resultado = document.getElementById('lista_productos');
+
+        
+
     }); // DOM Content Loaded
 })();
